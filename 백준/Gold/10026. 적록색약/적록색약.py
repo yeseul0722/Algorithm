490da6812524517df042ad5676f1454509f2d233
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-01-10 00:58:10 +0900

    [Gold V] Title: 적록색약, Time: 80 ms, Memory: 31660 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,63 +1,53 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10**6) # 파이썬의 재귀 깊이 지정 (Python3)
 input = sys.stdin.readline
+N = int(input())
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+graph = []
+visited = [[0] * (N) for _ in range(N)]
+sum = 0
+sum2 = 0
 
-def bfs1():
-    global cnt1
-    cnt1 += 1
-    while queue:
-        y, x = queue.popleft()
-        for k in range(4):
-            ny = y + dy[k]
-            nx = x + dx[k]
-            if 0 <= ny < n and 0 <= nx < n and not visited1[ny][nx]:
-                if board[y][x] == board[ny][nx]:
-                    queue.append((ny, nx))
-                    visited1[ny][nx] = True
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
+for i in range(N):
+    graph.append(list(input()))
 
-def bfs2():
-    global cnt2
-    cnt2 += 1
-    while queue:
-        y, x = queue.popleft()
-        for k in range(4):
-            ny = y + dy[k]
-            nx = x + dx[k]
-            if 0 <= ny < n and 0 <= nx < n and not visited2[ny][nx]:
-                if board[y][x] == board[ny][nx]:
-                    queue.append((ny, nx))
-                    visited2[ny][nx] = True
-                elif board[y][x] == 'R' or board[y][x] == 'G':
-                    if board[ny][nx] == 'R' or board[ny][nx] == 'G':
-                        queue.append((ny, nx))
-                        visited2[ny][nx] = True
+def dfs(x, y, color):
+    visited[x][y] = 1
 
+    # 동서남북 탐색
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        # 범위 내에 있고 방문한 적이 없다면
+        if 0 <= nx <= N-1 and 0 <= ny <= N-1 and visited[nx][ny] == 0:
+            # 위 조건을 모두 만족하면서 탐색중인 색상과 같은 색상이면 탐색
+            if graph[nx][ny] == color:
+                dfs(nx, ny, color)
 
-queue = deque()
-n = int(input())
-board = [list(input().rstrip()) for _ in range(n)]
-visited1 = [[False] * n for _ in range(n)]
-visited2 = [[False] * n for _ in range(n)]
-cnt1 = 0
-cnt2 = 0
+for color in ['R','G','B']: # 빨->초->파 순서로 확인
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] == 0 and graph[i][j] == color:
+                dfs(i, j,color)
+                sum += 1
 
-for y in range(n):
-    for x in range(n):
-        if not visited1[y][x]:
-            queue.append((y, x))
-            visited1[y][x] = True
-            bfs1()
+# 기존 그래프에서 초록색 -> 빨간색으로 변경
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 'G':
+            graph[i][j] = 'R'
 
-for y in range(n):
-    for x in range(n):
-        if not visited2[y][x]:
-            queue.append((y, x))
-            visited2[y][x] = True
-            bfs2()
+# 방문 정보 초기화
+visited = [[0] * (N) for _ in range(N)]
 
+for color in ['R', 'B']: # 빨,파만 확인
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] == 0 and graph[i][j] == color:
+                dfs(i, j,color)
+                sum2 += 1
 
-print(cnt1, cnt2)
+print(sum, sum2)
