49e2a305f2662b92461791d68c3f7409767f3e94
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-01-21 02:21:30 +0900

    [Silver III] Title: N과 M (7), Time: 2172 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+
+def recur(num):
+    if len(ans) == m:
+        print(*ans)
+        return
+    for i in range(n):
+        ans.append(lst[i])
+        recur(i)
+        ans.pop()
+
+
+n, m = map(int, input().split())
+lst = list(map(int, input().split()))
+lst.sort()
+ans = []
+recur(0)
