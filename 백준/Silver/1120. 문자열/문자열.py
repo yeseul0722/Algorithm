7331e5f84f31c8f47d9614adc5e5b5e5fd6864c7
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-02-17 23:17:24 +0900

    [Silver IV] Title: 문자열, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+a, b = input().split()
+ans = []
+for i in range(len(b) - len(a) + 1):
+    cnt = 0
+    for j in range(len(a)):
+        if a[j] != b[i + j]:
+            cnt += 1
+    ans.append(cnt)
+
+print(min(ans))
