9440d186f422573ad6c6cf3074043dd54b522795
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-02-19 23:23:51 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+exp = input().split('-')
+
+num = []
+
+for i in exp:
+    sum = 0
+    temp = i.split('+')
+    for j in temp:
+        sum += int(j)
+    num.append(sum)
+    
+n = num[0]
+
+for i in range(1, len(num)):
+    n -= num[i]
+
+print(n)
