ce2487b459054191a61fe36e06448da9636d12c2
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-02-25 22:32:00 +0900

    [Silver II] Title: 숫자판 점프, Time: 44 ms, Memory: 31632 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -5,6 +5,7 @@ input = sys.stdin.readline
 dy = [1, 0, -1, 0]
 dx = [0, 1, 0, -1]
 
+
 def dfs(y, x, number):
     if len(number) == 6:
         ans.append(number)
@@ -13,9 +14,7 @@ def dfs(y, x, number):
     for i in range(4):
         ny = y + dy[i]
         nx = x + dx[i]
-        if nx < 0 or nx >= 5 or ny < 0 or ny >= 5:
-            continue
-        else:
+        if 0 <= ny < 5 and 0 <= nx < 5:
             dfs(ny, nx, number + board[ny][nx])
 
 
