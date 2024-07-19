a01e25efc22fbc9fa8e2d3bd78bf3f21fc63580b
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-07-19 16:36:43 +0900

    [level 2] Title: 2 x n 타일링, Time: 11.75 ms, Memory: 11.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = [0] * (n + 1)
+    answer[1] = 1
+    answer[2] = 2
+    
+    if n >= 2:
+        for i in range(3, n + 1):
+            answer[i] = (answer[i - 1] + answer[i - 2])  % 1000000007
+        
+    return answer[n]
