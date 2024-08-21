da3cf9159e72b425fce720a906e5c8b135058002
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-08-21 14:46:44 +0900

    [Silver V] Title: 소트인사이드, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,15 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-n = input().rstrip()
-lst = []
-ans = []
-for i in n:
-    lst.append(int(i))
-
-lst.sort(reverse=True)
-
-for i in lst:
-    ans.append(str(i))
-
-print(''.join(ans))
+arr = list(map(str, input().rstrip()))
+arr.sort(reverse = True)
+print(int(''.join(arr)))
