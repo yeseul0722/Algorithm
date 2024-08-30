c022cd8be11bbd07275df0daad26ea82071fb8a8
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-08-31 02:06:24 +0900

    [level 2] Title: 귤 고르기, Time: 29.57 ms, Memory: 21.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,15 +1,13 @@
 from collections import Counter
 def solution(k, tangerine):
 
-    n = len(tangerine)
     dic = Counter(tangerine)
     size = []
     for key, value in dic.items():
         size.append(value)
-    size.sort()
+    size.sort(reverse = True)
+    
     for i in range(len(size)):
-        n -= size[i]
-        if n == k:
-            return len(size[i + 1:])
-        if n < k:
-            return len(size[i:])
+        k -= size[i]
+        if k <= 0:
+            return i + 1
