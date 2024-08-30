53d42fc47904f41a3714d62ee8e2de8c8148d2b1
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-08-31 01:43:02 +0900

    [level 2] Title: 방문 길이, Time: 2.47 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,14 +1,16 @@
+from collections import deque
 def solution(dirs):
-    visited = set()
-    direction =  {'U': (1, 0), 'D': (-1, 0), 'R': (0, 1), 'L': (0, -1)}
+    answer = 0
+    dir = {'U': (1, 0), 'D': (-1, 0), 'R': (0, 1), 'L': (0, -1)}
+    road = deque()
     y, x = 0, 0
     
-    for d in dirs:
-        dy, dx = direction[d]
-        ny, nx = y + dy, x + dx
-        if -5 <= ny < 6 and -5 <= nx < 6:
-            visited.add(((y, x), (ny, nx)))
-            visited.add(((ny, nx), (y, x)))
+    for i in dirs:
+        ny, nx = y + dir[i][0], x + dir[i][1]
+        if -5 <= ny <= 5 and -5 <= nx <= 5:
+            if not [(y, x), (ny, nx)] in road:
+                road.append([(y, x), (ny, nx)])
+                road.append([(ny, nx), (y, x)])
             y, x = ny, nx
             
-    return len(visited) // 2
+    return len(road) // 2
