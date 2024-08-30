7ef4599286677c1e63f53945027eeab8984e2bce
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-08-30 18:52:38 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.00 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 10일 22:55:34
+2024년 08월 30일 18:52:36
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,11 +1,13 @@
 def solution(n, lost, reserve):
+    
     reserve_set = set(reserve) - set(lost)
     lost_set = set(lost) - set(reserve)
-    
-    for r in reserve_set:
-        if r - 1 in lost_set:
-            lost_set.remove(r - 1)
-        elif r + 1 in lost_set:
-            lost_set.remove(r + 1)
             
-    return n-len(lost_set)
+    for i in reserve_set:
+        if i - 1 in lost_set:
+            lost_set.remove(i - 1)
+            continue
+        if i + 1 in lost_set:
+            lost_set.remove(i + 1)
+    
+    return n - len(lost_set)
