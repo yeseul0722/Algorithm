7eab1e8da623cec5b1c2b681f105c65bb0bfd349
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-09-03 01:42:31 +0900

    [level 2] Title: 미로 탈출, Time: 14.40 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,38 +1,38 @@
 from collections import deque
-
-def bfs(start, end, maps):
-    queue = deque()
-    n = len(maps[0])
-    m = len(maps)
-    visited = [[-1] * n for _ in range(m)]
-    flag = False
-    
-    dy = [1, 0, -1, 0]
-    dx = [0, 1, 0, -1]
-    
-    for y in range(m):
-        for x in range(n):
-            if maps[y][x] == start:
-                queue.append((y, x))
-                visited[y][x] = 0
-                break
-    
-    while queue:
-        y, x = queue.popleft()
-        if maps[y][x] == end:
-            return visited[y][x]
-        for k in range(4):
-            ny = y + dy[k]
-            nx = x + dx[k]
-            if 0 <= nx < n and 0 <= ny < m and visited[ny][nx] == -1 and maps[ny][nx] != 'X':
-                visited[ny][nx] = visited[y][x] + 1
-                queue.append((ny, nx))
-    return -1       
-
 def solution(maps):
-    path1 = bfs('S', 'L', maps)
-    path2 = bfs('L', 'E', maps)
-    if path1 != -1 and path2 != -1:
-        return path1 + path2
+    n = len(maps)
+    m = len(maps[0])
+    
+    def bfs(S, G):
+        dy = [1, 0, -1, 0]
+        dx = [0, 1, 0, -1]
+        visited = [[-1] * m for _ in range(n)]
+        queue = deque()
+        
+        for y in range(n):
+            for x in range(m):
+                if maps[y][x] == S:
+                    queue.append((y, x))
+                    visited[y][x] = 0
+                    break
+
+        while queue:
+            y, x = queue.popleft()
+            if maps[y][x] == G:
+                return visited[y][x]
+            for i in range(4):
+                ny = y + dy[i]
+                nx = x + dx[i]
+                if 0 <= ny < n and 0 <= nx < m and visited[ny][nx] == -1 and not maps[ny][nx] == 'X':
+                    queue.append((ny, nx))
+                    visited[ny][nx] = visited[y][x] + 1
+        return -1
+    
+    flag1 = bfs('S', 'L')
+    flag2 = bfs('L', 'E')
+    if flag1 != -1 and flag2 != -1:
+        return flag1 + flag2
     else:
         return -1
+
+    
