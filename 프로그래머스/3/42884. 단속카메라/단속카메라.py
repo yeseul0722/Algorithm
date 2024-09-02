ccbca59a507d3b175218662e06632e5a5c320397
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-09-03 01:16:11 +0900

    [level 3] Title: 단속카메라, Time: 0.85 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,10 +1,9 @@
 def solution(routes):
     answer = 0
+    end = -30001
     routes.sort(key = lambda x:x[1])
-    key = -30001
-    
     for route in routes:
-        if route[0] > key:
-            key = route[1]
+        if route[0] > end:
+            end = route[1]
             answer += 1
     return answer
