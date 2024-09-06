1c03922ee47dd0db5bcdcd8656a236aced975600
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-09-06 18:18:40 +0900

    [level 2] Title: 방문 길이, Time: 4.94 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,11 +1,10 @@
 def solution(dirs):
-    dir = {'U': (1, 0), 'D': (-1, 0), 'R': (0, 1), 'L': (0, -1)}
     answer = []
-    
+    direction = {'U': (1, 0), 'D': (-1, 0), 'R': (0, 1), 'L': (0, -1)}
     y, x = 0, 0
-    for d in dirs:
-        ny = y + dir[d][0]
-        nx = x + dir[d][1]
+    
+    for i in dirs:
+        ny, nx = y + direction[i][0], x + direction[i][1]
         if -5 <= ny <= 5 and -5 <= nx <= 5:
             if not [(y, x), (ny, nx)] in answer:
                 answer.append([(y, x), (ny, nx)])
