e895bf75ae98522ecbf1e32022a07e7663b7f27f
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-09-06 19:37:36 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 279.59 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -1,20 +1,19 @@
 def solution(arr):
-    n = len(arr)
     answer = [0, 0]
+    n = len(arr)
     
-    def press(start, end, n):
-        temp = arr[start][end]
-        for i in range(start, start + n):
-            for j in range(end, end + n):
+    def press(y, x, n):
+        temp = arr[y][x]
+        for i in range(y, y + n):
+            for j in range(x, x + n):
                 if arr[i][j] != temp:
                     n //= 2
-                    press(start, end, n)
-                    press(start, end + n, n)
-                    press(start + n, end, n)
-                    press(start + n, end + n, n)
+                    press(y, x, n)
+                    press(y, x + n, n)
+                    press(y + n, x, n)
+                    press(y + n, x + n, n)
                     return
         answer[temp] += 1
-    
     press(0, 0, n)
     
     return answer
