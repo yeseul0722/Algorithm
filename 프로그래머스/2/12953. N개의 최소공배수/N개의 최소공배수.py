0e30fe4a4366a5ef1656931e96506a79bb68c554
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-09-06 18:01:00 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,12 +1,8 @@
+import math
 def solution(arr):
-    a = arr[0]
-    lcm = arr[0]
-
-    for num in range(1, len(arr)):
-        b = arr[num]
-        while b > 0:
-            a, b = b, a % b
-        lcm = (lcm * arr[num]) // a
-        a = lcm
-
-    return lcm
+    answer = arr[0]
+    
+    for i in range(1, len(arr)):
+        answer = answer * arr[i] // math.gcd(answer, arr[i])
+        
+    return answer
