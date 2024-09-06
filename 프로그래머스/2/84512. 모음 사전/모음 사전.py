d6f473b1f35a7f7c9e5004b78766be0659873052
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-09-06 16:16:36 +0900

    [level 2] Title: 모음 사전, Time: 1.77 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,10 +1,12 @@
 from itertools import product
 def solution(word):
+    answer = 0
     vowels = ['A', 'E', 'I', 'O', 'U']
     dict = []
+    
     for i in range(1, 6):
-        for vowel in product(vowels, repeat = i):
-            dict.append(''.join(vowel))
-            
-    dict.sort()        
+        for p in product(vowels, repeat = i):
+            dict.append(''.join(p))
+    dict.sort()
+    
     return dict.index(word) + 1
