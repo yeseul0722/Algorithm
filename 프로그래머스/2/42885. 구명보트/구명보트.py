e88477c573e43cf9ab885cc1274adc4f35b0164f
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-09-07 01:57:56 +0900

    [level 2] Title: 구명보트, Time: 8.84 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,13 +1,14 @@
-from collections import deque
 def solution(people, limit):
     answer = 0
     people.sort(reverse = True)
-    queue = deque(people)
+    start, end = 0, len(people) - 1
     
-    while queue:
-        weight = queue.popleft()
-        if queue and queue[-1] + weight <= limit:
-            weight += queue.pop()
+    while start <= end:
+        if people[start] + people[end] <= limit:
+            start += 1
+            end -= 1
+        else:
+            start += 1
         answer += 1
-        
+    
     return answer
