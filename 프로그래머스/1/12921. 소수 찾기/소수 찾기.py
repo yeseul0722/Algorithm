66035c0c10d2c4503c429791bb55983cda2897af
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-11-27 16:07:14 +0900

    [level 1] Title: 소수 찾기, Time: 3053.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,15 +1,11 @@
 import math
 def solution(n):
     answer = 0
-
-    for i in range(2, n + 1):
-        flag = True
-        for j in range(2, int(math.sqrt(i)) + 1):
-            if i % j == 0:
-                flag = False
+    for num in range(2, n + 1):
+        for i in range(2, int(math.sqrt(num)) + 1):
+            if num % i == 0:
                 break
-                
-        if flag:
+        else:
             answer += 1
-                      
+            
     return answer
