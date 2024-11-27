9ff80bd6f761d3e82f22830a103d4b6750ee3639
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2024-11-27 15:27:31 +0900

    [level 2] Title: 피로도, Time: 53.35 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,14 +1,17 @@
 from itertools import permutations
 def solution(k, dungeons):
     answer = 0
-    
-    for permute in permutations(dungeons, len(dungeons)):
-        cnt = 0
+    n = len(dungeons)
+    for dungeon in permutations(dungeons, n):
         hp = k
-        for p in permute:
-            if hp >= p[0]:
-                hp -= p[1]
+        cnt = 0
+        for i in dungeon:
+            if hp >= i[0]:
+                hp -= i[1]
                 cnt += 1
-        answer = max(answer, cnt)
-        
+            else:
+                break
+        if cnt > answer:
+            answer = cnt
+                
     return answer
