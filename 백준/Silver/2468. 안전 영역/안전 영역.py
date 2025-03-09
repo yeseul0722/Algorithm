a18ef3012a9717e2a6cce03ea83243736eedc228
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2025-03-09 23:00:55 +0900

    [Silver I] Title: 안전 영역, Time: 860 ms, Memory: 34184 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+N = int(input())
+mat = []
+max_num = 0
+
+for i in range(N):
+    data = (list(map(int, input().split())))
+    mat.append(data)
+
+for i in range(N):
+    for j in range(N):
+        if mat[i][j] > max_num:
+            max_num = mat[i][j]
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+def bfs(a, b, value, visited):
+    q = deque()
+    q.append((a, b))
+    visited[a][b] = 1
+
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N:
+                if mat[nx][ny] > value and visited[nx][ny] == 0:
+                    visited[nx][ny] = 1
+                    q.append((nx, ny))
+
+
+result = 0
+for i in range(max_num):
+    visited = [[0] * N for _ in range(N)]
+    cnt = 0
+    for j in range(N):
+        for k in range(N):
+            if mat[j][k] > i and visited[j][k] == 0:
+                bfs(j, k, i, visited)
+                cnt += 1
+
+    if cnt > result:
+        result = cnt
+
+print(result)
