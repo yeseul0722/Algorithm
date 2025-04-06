da26cbac533b446137fc9ff1e70dfadcd207ae86
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2025-04-06 16:02:57 +0900

    [Silver I] Title: 영역 구하기, Time: 64 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,30 +1,30 @@
 import sys
+
 input = sys.stdin.readline
 from collections import deque
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
+
 def bfs(now):
     q = deque(now)
     tmp_width = 0
     while q:
         y, x = q.popleft()
-        visited[y][x] = True
+        board[y][x] = 1
         tmp_width += 1
         for k in range(4):
             ny = y + dy[k]
             nx = x + dx[k]
-            if 0 <= ny < m and 0 <= nx < n:
-                if board[ny][nx] == 0 and not visited[ny][nx]:
-                    q.append([ny, nx])
-                    visited[ny][nx] = True
+            if 0 <= ny < m and 0 <= nx < n and board[ny][nx] == 0:
+                q.append([ny, nx])
+                board[ny][nx] = 1
     return tmp_width
 
 
 m, n, k = map(int, input().split())
 board = [[0] * n for _ in range(m)]
-visited = [[False] * n for _ in range(m)]
 width_lst = []
 cnt = 0
 
@@ -36,12 +36,11 @@ for _ in range(k):
 
 for y in range(m):
     for x in range(n):
-        if board[y][x] == 0 and not visited[y][x]:
+        if board[y][x] == 0:
             width = bfs([[y, x]])
             width_lst.append(width)
             cnt += 1
 
 width_lst.sort()
 print(cnt)
-print(*width_lst)
-
+print(' '.join(map(str, width_lst)))
