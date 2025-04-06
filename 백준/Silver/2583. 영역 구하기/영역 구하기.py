5e23524d5ee56ae127e6838600c3c219b39308d6
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2025-04-06 15:51:16 +0900

    [Silver I] Title: 영역 구하기, Time: 64 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,54 +1,47 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
-queue = deque()
+from collections import deque
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-
-def bfs():
-    global temp
-    while queue:
-        y, x = queue.popleft()
-        temp += 1
+def bfs(now):
+    q = deque(now)
+    tmp_width = 0
+    while q:
+        y, x = q.popleft()
+        visited[y][x] = True
+        tmp_width += 1
         for k in range(4):
             ny = y + dy[k]
             nx = x + dx[k]
-            if 0 <= ny < m and 0 <= nx < n and not visited[ny][nx]:
-                if maze[ny][nx] == 0:
+            if 0 <= ny < m and 0 <= nx < n:
+                if board[ny][nx] == 0 and not visited[ny][nx]:
+                    q.append([ny, nx])
                     visited[ny][nx] = True
-                    queue.append((ny, nx))
-    if temp <= 1:
-        return temp
-    else:
-        return temp - 1
+    return tmp_width
 
 
 m, n, k = map(int, input().split())
-maze = [[0] * n for _ in range(m)]
+board = [[0] * n for _ in range(m)]
 visited = [[False] * n for _ in range(m)]
+width_lst = []
 cnt = 0
-width = []
-for _ in range(k):
-    sx, sy, ex, ey = map(int, input().split())
-    for y in range(sy, ey):
-        for x in range(sx, ex):
-            maze[y][x] = 1
 
+for _ in range(k):
+    lx, ly, rx, ry = map(int, input().split())
+    for y in range(ly, ry):
+        for x in range(lx, rx):
+            board[y][x] = 1
 
 for y in range(m):
     for x in range(n):
-        temp = 0
-        if maze[y][x] == 0 and not visited[y][x]:
-            queue.append((y, x))
-            temp = bfs()
+        if board[y][x] == 0 and not visited[y][x]:
+            width = bfs([[y, x]])
+            width_lst.append(width)
             cnt += 1
-            if temp > 0:
-                width.append(temp)
 
-width.sort()
+width_lst.sort()
 print(cnt)
-print(*width)
+print(*width_lst)
 
