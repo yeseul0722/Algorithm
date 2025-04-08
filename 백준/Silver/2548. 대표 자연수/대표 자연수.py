f0c6d27cb5ba371f463e4b150d14fabeafc5cdfe
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2025-04-08 11:51:29 +0900

    [Silver III] Title: 대표 자연수, Time: 36 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Silver/2548. 대표 자연수/대표 자연수.py
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.py
@@ -5,16 +5,4 @@ n = int(input())
 num_lst = list(map(int, input().split()))
 num_lst.sort()
 
-if n % 2 == 1:
-    print(num_lst[(n - 1) // 2])
-else:
-    l, r = num_lst[(n - 1) // 2], num_lst[(n - 1) // 2 + 1]
-    l_tmp, r_tmp = 0, 0
-    for i in range(n):
-        l_tmp += abs(num_lst[i] - l)
-        r_tmp += abs(num_lst[i] - r)
-    if l_tmp >= r_tmp:
-        print(l)
-    else:
-        print(r)
-
+print(num_lst[(n - 1) // 2])
