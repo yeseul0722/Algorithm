ba702f82f8c084e3e9f6422f42703dc68eebcce3
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2025-04-24 01:19:25 +0900

    [Silver I] Title: 그림, Time: 292 ms, Memory: 36404 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -3,9 +3,10 @@ from collections import deque
 
 input = sys.stdin.readline
 
+
 def bfs(now):
     q = deque([now])
-    tmp = 1
+    size = 1
     while q:
         y, x = q.popleft()
         for k in range(4):
@@ -14,18 +15,19 @@ def bfs(now):
             if 0 <= ny < n and 0 <= nx < m:
                 if not visited[ny][nx] and board[ny][nx] == 1:
                     visited[ny][nx] = True
-                    tmp += 1
+                    size += 1
                     q.append([ny, nx])
-    return tmp
+    return size
+
 
 board = []
-ans = []
+cnt = 0
+max_num = 0
 dy = [1, 0, -1, 0]
 dx = [0, 1, 0, -1]
 
 n, m = map(int, input().split())
-for _ in range(n):
-    board.append(list(map(int, input().split())))
+board = [list(map(int, input().split())) for _ in range(n)]
 
 visited = [[False] * m for _ in range(n)]
 
@@ -33,11 +35,8 @@ for y in range(n):
     for x in range(m):
         if not visited[y][x] and board[y][x] == 1:
             visited[y][x] = True
-            cnt = bfs([y, x])
-            ans.append(cnt)
+            cnt += 1
+            max_num =  max(bfs([y, x]), max_num)
 
-print(len(ans))
-if not ans:
-    print(0)
-else:
-    print(max(ans))
+print(cnt)
+print(max_num)
