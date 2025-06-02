ae7d3a65cfc6168c6d6eb78e19d36afa211d3651
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2025-06-02 14:33:55 +0900

    [Silver III] Title: 거듭제곱, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1740. 거듭제곱/거듭제곱.py
@@ -0,0 +1,9 @@
+n = int(input())
+bin_num = bin(n)
+bin_num = bin_num[2:] # 이진수로 변환했기 때문에 0b 제거
+ans = 0
+for i in range(len(bin_num)):
+    if bin_num[i] == "1": # 1이 있으면 
+       temp = 3 ** (len(bin_num)-i-1) # 이진수로 따지면 3의 제곱자리임
+       ans +=  temp # 답에 더해주기
+print(ans)
