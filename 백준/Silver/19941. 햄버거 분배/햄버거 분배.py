8fa10863a9f8825d1431d8839857756c416be125
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2025-07-16 17:13:06 +0900

    [Silver III] Title: 햄버거 분배, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+placement = list(input())
+ans = 0
+for idx in range(n):
+    if placement[idx] == 'P':
+        for i in range(max(idx-k, 0), min(idx+k+1, n)):
+            if placement[i] == 'H':
+                placement[i] = 0
+                ans += 1
+                break
+print(ans)
