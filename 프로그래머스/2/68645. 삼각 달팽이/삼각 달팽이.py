84605fcd5c64077dfe1e9ef9b780c93b2f25b535
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2025-08-11 17:57:41 +0900

    [level 2] Title: 삼각 달팽이, Time: 188.30 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -4,8 +4,8 @@ def solution(n):
     dx = [0, 1, -1]
     dy = [1, 0, -1]
     end_num = 0
-    x, y = 0, 0
     dist = 0
+    x, y = 0, 0
     
     for i in range(1, n + 1):
         snail.append([0] * i)
@@ -13,19 +13,21 @@ def solution(n):
     
     for num in range(1, end_num + 1):
         snail[y][x] = num
+        
         y += dy[dist]
         x += dx[dist]
         
         if y < 0 or x < 0 or y >= n or x >= len(snail[y]) or snail[y][x] != 0:
+            
             y -= dy[dist]
             x -= dx[dist]
-            
+
             dist = (dist + 1) % 3
-            
+
             y += dy[dist]
             x += dx[dist]
-    
+
     for row in range(n):
         answer += snail[row]
-        
+            
     return answer
