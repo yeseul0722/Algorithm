b41f255b11ec998d6bb307c4c384f46718a3a5a5
김예슬 <108119868+yeseul0722@users.noreply.github.com>
2025-09-12 21:35:53 +0900

    [level 2] Title: 타겟 넘버, Time: 193.49 ms, Memory: 48.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,10 @@
 def solution(numbers, target):
-    # 경우의 수 담을 리스트 만들어주기
     lst = [0]
-
+    
     for i in range(len(numbers)):
-        # 이전의 수에 현재 수를 +,- 연산을 수행하며
-        # 모든 경우의 수를 임시 리스트에 담아주기
-        temp = []
+        tmp = []
         for j in lst:
-            temp.append(j + numbers[i])
-            temp.append(j - numbers[i])
-        lst = temp
-        
-        # target과 일치하는 수 반환하기
+            tmp.append(j + numbers[i])
+            tmp.append(j - numbers[i])
+        lst = tmp
     return lst.count(target)
